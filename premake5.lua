4f5fa0267741b61594d83d3a8a2b6ba839c9bd38
GatoImorrivel <guilhermevianagui23@gmail.com>
2022-09-27 01:32:46 -0300

    sofrendo com o docker

---
+++ premake5.lua
@@ -0,0 +1,18 @@
+workspace "particle-sim"
+    configurations {"Debug", "Release"}
+
+project "particle-sim"
+    kind "WindowedApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+    links { "sfml-graphics", "sfml-window", "sfml-system" }
+
+    files {"./src/**.hpp", "./src/**.cpp"}
+
+    filter "configurations:Debug"
+        defines {"DEBUG"}
+        symbols "On"
+
+    filter "configurations:Release"
+        defines {"NDEBUG"}
+        optimize "On"
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1260, 848, 32), "Teste");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Red);
+        window.display();
+    }
+
+    return 0;
+}
